f4bd6f55c84a29d709217f90dfabc1055942b54d
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-09-20 21:55:19 -0300

    Inicio do projeto

---
+++ app/src/main/java/br/edu/infnet/app_quiz_assessment/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.app_quiz_assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
