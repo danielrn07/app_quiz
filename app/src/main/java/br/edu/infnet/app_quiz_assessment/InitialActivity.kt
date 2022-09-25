487dc8f120b6ccbede7331122b158a67c305a792
Daniel Nascimento <pdaniel.contato@gmail.com>
2022-09-25 15:58:25 -0300

    Implementa dialog com informações sobre o aplicativo

--- app/src/main/java/br/edu/infnet/app_quiz_assessment/InitialActivity.kt
+++ app/src/main/java/br/edu/infnet/app_quiz_assessment/InitialActivity.kt
@@ -1,9 +1,12 @@
 package br.edu.infnet.app_quiz_assessment
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.edu.infnet.app_quiz_assessment.databinding.ActivityInitialBinding
 import br.edu.infnet.app_quiz_assessment.databinding.ActivityMainBinding
+import br.edu.infnet.app_quiz_assessment.databinding.CustomBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class InitialActivity : AppCompatActivity() {
 
@@ -15,9 +18,29 @@ class InitialActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        setup()
+
+    }
+
+    private fun setup() {
+        configureButtons()
+    }
+
+    private fun showBottomSheetDialog() {
+        val dialog = BottomSheetDialog(this, R.style.BottomSheetDialog)
+        val sheetBinding: CustomBottomSheetBinding =
+            CustomBottomSheetBinding.inflate(layoutInflater, null, false)
+        dialog.setContentView(sheetBinding.root)
+        dialog.show()
+    }
+
+    private fun configureButtons() {
         binding.btnIniciar.setOnClickListener {
             val intent = Intent(this, QuestionsActivity::class.java)
             startActivity(intent)
+        }
+        binding.btnSobre.setOnClickListener {
+            showBottomSheetDialog()
         }
     }
 }
